7ba07c5c5125c4cc3f118d9acbd59c155e465551
hegej <hege.jacobsen12@gmail.com>
2024-02-09 09:20:39 +0100

    Added session storage for keeping generated report in the browser session

--- CustomerReportTest/Program.cs
+++ CustomerReportTest/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.SessionStorage;
 using CustomerReportTest.Components;
 using CustomerReportTest.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,24 +11,25 @@ builder.Services.AddHttpClient<OrganizationService>(client =>
 });
 
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<OrganizationService>();
+builder.Services.AddBlazoredSessionStorage();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
